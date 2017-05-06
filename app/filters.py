7b37c92cc2173a1afd069230d9d82ff8bdf94d28
Tim van den Bosch <tim@afriendofmine.nl>
2017-05-06 10:57:08 +0200

    Removed debugging code

--- app/filters.py
+++ app/filters.py
@@ -1,10 +1,8 @@
 # app/filters.py
+
 from datetime import datetime
 
 def timeago(dt, default="Nu geplaatst"):
-
-	#return repr(dt.hour)
-	#return repr(datetime.utcnow())
 
     now = datetime.now()
     diff = now - dt
@@ -25,23 +23,4 @@ def timeago(dt, default="Nu geplaatst"):
             return "%d %s" % (period, singular if period == 1 else plural)
 
     return default
-
-
-
-# def string_to_data_time(d):
-
-# 	d = d.replace('/', '-')
-# 	if ' ' in d:
-# 		_datetime = d.split(' ')
-# 		if len(_datetime) == 2:
-# 			_d = _string_to_date(_datetime[0])
-# 			_t = _string_to_time(_datetime[1])
-# 			return _combine_date_time(_d, _t)
-
-# 	return None
-
-# def _combine_date_time(d, t):
-# 	if (d is not None) and (t is not None):
-# 		return datetime(d.year, d.month, d.day, t.hour, t.minute, t.second)
-# 	return None
-	
+    
